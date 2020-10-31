b7ae2d8ff765fa5771314abcc634f48b4ed9cfb7
Pavel Ivanov <pafelka@gmail.com>
2020-10-31 09:59:29 +0200

    [v1.2.0] - use less package dependencies

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-global.BigInt = require("bigint-polyfill");
 global.fetch = require("node-fetch");
 
-const tmImage = require("@teachablemachine/image");
+const { Readable } = require('stream');
+const tf = require('@tensorflow/tfjs')
+const PImage = require('pureimage')
 const isImageUrl = require('is-image-url');
-const canvas = require("canvas");
-
-const { JSDOM } = require("jsdom");
-const dom = new JSDOM("");
-global.document = dom.window.document;
-global.HTMLVideoElement = dom.window.HTMLVideoElement;
-
 
 const wait = ms => new Promise(r => setTimeout(r, ms));
 
@@ -30,21 +24,67 @@ const retryOperation = (operation, delay, times) => new Promise((resolve, reject
     });
 });
 
-const getHTMLCanvasElement = (imageUrl) => {
-  return new Promise((resolve, reject) => {
-    const image = new canvas.Image();
-    image.onload = () => resolve(image);
-    image.onerror = (e) => reject(e);
-    image.src = imageUrl;
+const bufferToStream = (binary) => {
+  const readableInstanceStream = new Readable({
+    read() {
+      this.push(binary)
+      this.push(null);
+    }
   });
+
+  return readableInstanceStream;
 }
 
-const byProbabilty = (predictionA, predictionB) => {
-  if (predictionA.probability > predictionB.probability) return -1;
-  if (predictionA.probability < predictionB.probability) return 1;
-  return 0;
+const predict = async (imgElement, model) => {
+  const logits = tf.tidy(() => {
+    // tf.browser.fromPixels() returns a Tensor from an image element.
+    let img = tf.browser.fromPixels(imgElement).toFloat();
+    img = tf.image.resizeNearestNeighbor(img, [model.inputs[0].shape[1], model.inputs[0].shape[2]]);
+
+    const offset = tf.scalar(127.5);
+    // Normalize the image from [0, 255] to [-1, 1].
+    const normalized = img.sub(offset).div(offset);
+
+    // Reshape to a single-element batch so we can pass it to predict.
+    const batched = normalized.reshape([1, model.inputs[0].shape[1], model.inputs[0].shape[2], model.inputs[0].shape[3]]);
+
+    return model.predict(batched);
+  });
+
+  const predictions = await getTopKClasses(logits, model.classes);
+
+  return predictions;
 }
 
+const getTopKClasses = async (logits, classes) => {
+  const values = await logits.data();
+  const topK = Math.min(classes.length, values.length);
+
+  const valuesAndIndices = [];
+  for (let i = 0; i < values.length; i++) {
+    valuesAndIndices.push({ value: values[i], index: i });
+  }
+
+  valuesAndIndices.sort((a, b) => {
+    return b.value - a.value;
+  });
+
+  const topkValues = new Float32Array(topK);
+  const topkIndices = new Int32Array(topK);
+  for (let i = 0; i < topK; i++) {
+    topkValues[i] = valuesAndIndices[i].value;
+    topkIndices[i] = valuesAndIndices[i].index;
+  }
+
+  const topClassesAndProbs = []
+  for (let i = 0; i < topkIndices.length; i++) {
+    topClassesAndProbs.push({
+      class: classes[topkIndices[i]],
+      score: topkValues[i]
+    });
+  }
+  return topClassesAndProbs;
+}
 
 class SashiDoTeachableMachine {
   constructor(params) {
@@ -59,7 +99,13 @@ class SashiDoTeachableMachine {
     }
 
     try {
-      this.model = await tmImage.load(`${modelUrl}model.json`, `${modelUrl}metadata.json`);
+      const modelURL = `${modelUrl}model.json`;
+      const response = await fetch(`${modelUrl}metadata.json`)
+      const body = await response.text();
+      this.model = await tf.loadLayersModel(modelURL);
+      this.model.classes = JSON.parse(body).labels;
+      // console.log('@@@', this.model)
+
     } catch (e) {
       console.error("[@sashido/teachablemachine-node] -", e);
     }
@@ -92,9 +138,23 @@ class SashiDoTeachableMachine {
 
   async inference({ imageUrl }) {
     try {
-      const image = await getHTMLCanvasElement(imageUrl);
-      const predictions = await this.model.predict(image);
-      return predictions.sort(byProbabilty);
+      const data = await fetch(imageUrl);
+
+      const contentType = data.headers.get("Content-Type");
+      const buffer = await data.buffer();
+      const stream = bufferToStream(buffer);
+      let imageBitmap;
+
+      if ((/png/).test(contentType)) {
+        imageBitmap = await PImage.decodePNGFromStream(stream);
+      }
+
+      if ((/jpe?g/).test(contentType)) {
+        imageBitmap = await PImage.decodeJPEGFromStream(stream);
+      }
+
+      const predictions = await predict(imageBitmap, this.model);
+      return predictions;
     } catch (error) {
       return Promise.reject({ error });
     }
